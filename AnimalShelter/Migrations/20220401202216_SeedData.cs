b4ced55226eb71cc248f56e2e1309efc48c82709
jakechaley <jakechaley@gmail.com>
2022-04-01 13:24:41 -0700

    Seed Animal Data

---
+++ AnimalShelter/Migrations/20220401202216_SeedData.cs
@@ -0,0 +1,140 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AnimalShelter.Migrations
+{
+    public partial class SeedData : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Animals",
+                columns: new[] { "AnimalId", "Age", "Breed", "Gender", "Name", "Species" },
+                values: new object[,]
+                {
+                    { 1, 13, "Celtic Setter", "Female", "Sophie", "Dog" },
+                    { 18, 3, "Sphynx", "Female", "LeeLoo", "Cat" },
+                    { 17, 4, "Siamese", "Male", "Zero", "Cat" },
+                    { 16, 1, "Bengal", "Male", "Oliver", "Cat" },
+                    { 15, 7, "Tabby", "Female", "Oona", "Cat" },
+                    { 14, 6, "Russian Blue", "Male", "Dave", "Cat" },
+                    { 13, 5, "Scottish Fold", "Male", "Beans", "Cat" },
+                    { 12, 4, "Maine Coon", "Male", "Dog", "Cat" },
+                    { 11, 12, "Orange Tabby", "Female", "Charlie", "Cat" },
+                    { 10, 5, "Beagle", "Female", "Lucky", "Dog" },
+                    { 9, 6, "Dalmation", "Female", "Spot", "Dog" },
+                    { 8, 3, "Bull Terrier", "Female", "Zoie", "Dog" },
+                    { 7, 4, "Poodle", "Male", "Marcus", "Dog" },
+                    { 6, 1, "Golden Retriever", "Male", "Zane", "Dog" },
+                    { 5, 7, "Pitbull", "Female", "Penny", "Dog" },
+                    { 4, 6, "Jack Russel Terrier", "Male", "Gary2", "Dog" },
+                    { 3, 5, "Coon Hound", "Male", "Lawrence", "Dog" },
+                    { 2, 4, "English Bulldog", "Male", "Bucket", "Dog" },
+                    { 19, 6, "Oriental Shorthair", "Female", "Madame Princess Cat", "Cat" },
+                    { 20, 5, "Persian", "Female", "Boo", "Cat" }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Animals",
+                keyColumn: "AnimalId",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Animals",
+                keyColumn: "AnimalId",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Animals",
+                keyColumn: "AnimalId",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Animals",
+                keyColumn: "AnimalId",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Animals",
+                keyColumn: "AnimalId",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "Animals",
+                keyColumn: "AnimalId",
+                keyValue: 6);
+
+            migrationBuilder.DeleteData(
+                table: "Animals",
+                keyColumn: "AnimalId",
+                keyValue: 7);
+
+            migrationBuilder.DeleteData(
+                table: "Animals",
+                keyColumn: "AnimalId",
+                keyValue: 8);
+
+            migrationBuilder.DeleteData(
+                table: "Animals",
+                keyColumn: "AnimalId",
+                keyValue: 9);
+
+            migrationBuilder.DeleteData(
+                table: "Animals",
+                keyColumn: "AnimalId",
+                keyValue: 10);
+
+            migrationBuilder.DeleteData(
+                table: "Animals",
+                keyColumn: "AnimalId",
+                keyValue: 11);
+
+            migrationBuilder.DeleteData(
+                table: "Animals",
+                keyColumn: "AnimalId",
+                keyValue: 12);
+
+            migrationBuilder.DeleteData(
+                table: "Animals",
+                keyColumn: "AnimalId",
+                keyValue: 13);
+
+            migrationBuilder.DeleteData(
+                table: "Animals",
+                keyColumn: "AnimalId",
+                keyValue: 14);
+
+            migrationBuilder.DeleteData(
+                table: "Animals",
+                keyColumn: "AnimalId",
+                keyValue: 15);
+
+            migrationBuilder.DeleteData(
+                table: "Animals",
+                keyColumn: "AnimalId",
+                keyValue: 16);
+
+            migrationBuilder.DeleteData(
+                table: "Animals",
+                keyColumn: "AnimalId",
+                keyValue: 17);
+
+            migrationBuilder.DeleteData(
+                table: "Animals",
+                keyColumn: "AnimalId",
+                keyValue: 18);
+
+            migrationBuilder.DeleteData(
+                table: "Animals",
+                keyColumn: "AnimalId",
+                keyValue: 19);
+
+            migrationBuilder.DeleteData(
+                table: "Animals",
+                keyColumn: "AnimalId",
+                keyValue: 20);
+        }
+    }
+}
--- AnimalShelter/Models/AnimalShelterContext.cs
+++ AnimalShelter/Models/AnimalShelterContext.cs
@@ -9,6 +9,32 @@ namespace AnimalShelter.Models
         {
         }
 
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<Animal>()
+            .HasData(
+                new Animal { AnimalId = 1, Name = "Sophie", Species = "Dog", Age = 13, Gender = "Female", Breed = "Celtic Setter"},
+                new Animal { AnimalId = 2, Name = "Bucket", Species = "Dog", Age = 4, Gender = "Male", Breed = "English Bulldog"},
+                new Animal { AnimalId = 3, Name = "Lawrence", Species = "Dog", Age = 5, Gender = "Male", Breed = "Coon Hound"},
+                new Animal { AnimalId = 4, Name = "Gary2", Species = "Dog", Age = 6, Gender = "Male", Breed = "Jack Russel Terrier"},
+                new Animal { AnimalId = 5, Name = "Penny", Species = "Dog", Age = 7, Gender = "Female", Breed = "Pitbull"},
+                new Animal { AnimalId = 6, Name = "Zane", Species = "Dog", Age = 1, Gender = "Male", Breed = "Golden Retriever"},
+                new Animal { AnimalId = 7, Name = "Marcus", Species = "Dog", Age = 4, Gender = "Male", Breed = "Poodle"},
+                new Animal { AnimalId = 8, Name = "Zoie", Species = "Dog", Age = 3, Gender = "Female", Breed = "Bull Terrier"},
+                new Animal { AnimalId = 9, Name = "Spot", Species = "Dog", Age = 6, Gender = "Female", Breed = "Dalmation"},
+                new Animal { AnimalId = 10, Name = "Lucky", Species = "Dog", Age = 5, Gender = "Female", Breed = "Beagle"},
+                new Animal { AnimalId = 11, Name = "Charlie", Species = "Cat", Age = 12, Gender = "Female", Breed = "Orange Tabby"},
+                new Animal { AnimalId = 12, Name = "Dog", Species = "Cat", Age = 4, Gender = "Male", Breed = "Maine Coon"},
+                new Animal { AnimalId = 13, Name = "Beans", Species = "Cat", Age = 5, Gender = "Male", Breed = "Scottish Fold"},
+                new Animal { AnimalId = 14, Name = "Dave", Species = "Cat", Age = 6, Gender = "Male", Breed = "Russian Blue"},
+                new Animal { AnimalId = 15, Name = "Oona", Species = "Cat", Age = 7, Gender = "Female", Breed = "Tabby"},
+                new Animal { AnimalId = 16, Name = "Oliver", Species = "Cat", Age = 1, Gender = "Male", Breed = "Bengal"},
+                new Animal { AnimalId = 17, Name = "Zero", Species = "Cat", Age = 4, Gender = "Male", Breed = "Siamese"},
+                new Animal { AnimalId = 18, Name = "LeeLoo", Species = "Cat", Age = 3, Gender = "Female", Breed = "Sphynx"},
+                new Animal { AnimalId = 19, Name = "Madame Princess Cat", Species = "Cat", Age = 6, Gender = "Female", Breed = "Oriental Shorthair"},
+                new Animal { AnimalId = 20, Name = "Boo", Species = "Cat", Age = 5, Gender = "Female", Breed = "Persian"}
+                );
+        }
         public DbSet<Animal> Animals { get; set; }
     }
 }
